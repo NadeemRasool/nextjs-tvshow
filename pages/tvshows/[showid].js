d1bd93770bec6ffdea6a6bddba7ce561606f45c6
Nadeem Rasool <nadeem_rasool@outlook.com>
2022-03-25 15:32:22 +0530

    adding keys in detail

--- pages/tvshows/[showid].js
+++ pages/tvshows/[showid].js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import styles from '../../styles/Home.module.css'
+import Link from 'next/link'
 export async function getStaticProps(context){
     let showId=context.params.showid;
     let show = await fetch(`https://api.tvmaze.com/shows/${showId}`);
@@ -22,20 +23,23 @@ export async function getStaticPaths(context){
 }
 
 export default function ShowDetail({show}){
-    console.log(show);
-    return (
-        <div className={styles.fx_r} style={{margin:10,justifyContent:'flex-start'}}>      
-            <div>
-                <img src={show.image.original} className={styles.showImg}/>
-            </div>
-            <div className={styles.fx_c} style={{width:'70%',paddingLeft:10,alignItems:'flex-start'}}>
-                <div style={{padding:10}}>{show.name}</div>
-                <div style={{padding:10}} className={styles.fx_r}>
-                    {show.genres.map(genre=>(<div style={{paddingRight:10}}>{genre}</div>))}
-                </div>
-                <div style={{padding:10}}>{show.summary}</div>
 
-            </div>      
+    return (
+        <div className={styles.fx_c}>
+            <Link style={{padding:10}} href={`/`}>Back</Link>
+            <div className={styles.fx_r} style={{margin:10,justifyContent:'flex-start'}}>      
+                <div>
+                    <img src={show.image.original} className={styles.showImg}/>
+                </div>
+                <div className={styles.fx_c} style={{width:'70%',paddingLeft:10,alignItems:'flex-start'}}>
+                    <div style={{padding:10}}>{show.name}</div>
+                    <div style={{padding:10}} className={styles.fx_r}>
+                        {show.genres.map(genre=>(<div style={{paddingRight:10}} key={genre}>{genre}</div>))}
+                    </div>
+                    <div style={{padding:10}}>{show.summary}</div>
+
+                </div>      
+            </div>
         </div>
     );
 }
