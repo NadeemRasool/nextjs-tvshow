b2fd7859e7a576c64742eb16b04388a44324daaf
Nadeem Rasool <nadeem_rasool@outlook.com>
2022-03-25 14:58:15 +0530

    show detail pages added

---
+++ pages/tvshows/[showid].js
@@ -0,0 +1,41 @@
+import { useRouter } from "next/router";
+import styles from '../../styles/Home.module.css'
+export async function getStaticProps(context){
+    let showId=context.params.showid;
+    let show = await fetch(`https://api.tvmaze.com/shows/${showId}`);
+    show=await show.json();
+    return ({
+        props:{
+            show
+        }
+    });
+}
+
+export async function getStaticPaths(context){
+    let showList = await fetch("https://api.tvmaze.com/search/shows?q=a");
+    showList=await showList.json();
+    let showId=showList.map(path=>({params:{showid:path.show.id.toString()}}));
+    return ({
+        paths:showId,
+        fallback:false
+    });
+}
+
+export default function ShowDetail({show}){
+    console.log(show);
+    return (
+        <div className={styles.fx_r} style={{margin:10,justifyContent:'flex-start'}}>      
+            <div>
+                <img src={show.image.original} className={styles.showImg}/>
+            </div>
+            <div className={styles.fx_c} style={{width:'70%',paddingLeft:10,alignItems:'flex-start'}}>
+                <div style={{padding:10}}>{show.name}</div>
+                <div style={{padding:10}} className={styles.fx_r}>
+                    {show.genres.map(genre=>(<div style={{paddingRight:10}}>{genre}</div>))}
+                </div>
+                <div style={{padding:10}}>{show.summary}</div>
+
+            </div>      
+        </div>
+    );
+}
